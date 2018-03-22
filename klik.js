f85e83cec6f672c86ff3a280826ae8f405bd1c9e
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-03-22 09:01:37 +0100

    Update klik.js

--- klik.js
+++ klik.js
@@ -3,16 +3,24 @@
 const http = require('http');
 const spawn = require('child_process').spawn;
 const server = http.createServer();
-function klik(req, res) {
-	const args = ['-rot','180','-w','800','-h','600','-a','92','-t','1','-q','95','--thumb','none','-mm','matrix','-drc','high','-n','-o','-'];  
+function klik(req, res, typ) {
+	let args = [];  
+	if (typ===0) args = ['-rot','180','-w','800','-h','600','-t','1','-q','95','--thumb','none','-mm','matrix','-drc','high','-n','-o','-'];  
+	if (typ===1) args = ['-rot','180','-w','800','-h','600','-a','92','-t','1','-q','95','--thumb','none','-mm','matrix','-drc','high','-n','-o','-'];  
 	const proc = spawn('raspistill', args);
 	proc.stdout.pipe(res);
 }
 server.on('request', (req, res) => {
-	if (req.url === '/klik') {
-		klik(req, res);
-	} else {
-		res.end('Ok');
-	}
+	console.log(req.url);
+	if (req.url === '/') {
+		klik(req, res, 1);
+	} 
+	else if (req.url === '/klik') {
+		klik(req, res, 0);
+	} 
+	else if (req.url === '/test') {
+		klik(req, res, 1);
+	} 
+	else {res.end('Ok');}
 });
 server.listen(3333,function(){console.log(':3333')});
